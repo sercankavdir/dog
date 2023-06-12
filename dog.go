b4daa3dbc459cb086910f738c39fa0c7530dceda
Sercan Kavdır <sercan.kavdir@maglab.com.tr>
2023-06-12 11:30:24 +0300

    changes

--- dog.go
+++ dog.go
@@ -3,5 +3,5 @@ package dog
 import "strings"
 
 func WhenGrowUp(s string) string {
-	return "When puppy grows up it says" + strings.ToUpper(s)
+	return "When puppy grows up it says " + strings.ToUpper(s)
 }
